e13618441228aa96148207a37ad36fc4bbb1ead3
pooba <celianmichez@free.fr>
2024-08-22 16:23:12 +0200

    oui

--- srcs/requirements/mariadb/conf/setup.sh
+++ srcs/requirements/mariadb/conf/setup.sh
@@ -1,13 +1,40 @@
 #!/bin/sh
 
-/usr/bin/mysql_install_db --user=root --basedir=/usr --datadir=/var/lib/mysql
-/usr/bin/mysqld --user=root --datadir=/var/lib/mysql & sleep 2
+if [ ! -d "/run/mysqld" ]; then
+	mkdir -p /run/mysqld
+	chown -R mysql:mysql /run/mysqld
+	chmod 777 /run/mysqld
+fi
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${MB_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${MB_USER}\`@'localhost' IDENTIFIED BY '${MB_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${MB_DATABASE}\`.* TO \`${MB_USER}\`@'%' IDENTIFIED BY '${MB_PASSWORD}';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MB_ROOT_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGES;"
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+	mkdir -p /var/lib/mysql
+	chown -R mysql:mysql /var/lib/mysql
 
-pkill mysqld
-/usr/bin/mysqld --user=root --datadir=/var/lib/mysql
+	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm
+
+	cat << EOF > tmp.sql
+
+USE mysql;
+FLUSH PRIVILEGES;
+
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MB_ROOT_PASSWORD';
+
+CREATE DATABASE $MB_DATABASE CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER '$MB_USER'@'%' IDENTIFIED BY '$MB_PASSWORD';
+GRANT ALL PRIVILEGES ON $MB_DATABASE.* TO '$MB_USER'@'%';
+
+FLUSH PRIVILEGES;
+EOF
+	
+	/usr/bin/mysqld --user=mysql --bootstrap < tmp.sql
+	rm -rf /tmp.sql
+fi
+
+#kill $(cat /var/run/mysqld/mysqld.pid)
+sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
+sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
+
+exec /usr/bin/mysqld --user=mysql --console
--- srcs/requirements/wordpress/conf/setup.sh
+++ srcs/requirements/wordpress/conf/setup.sh
@@ -1,37 +1,18 @@
 #!/bin/sh
 
-mkdir -p /var/www/
-mkdir -p /var/www/html
-
-cd /var/www/html
-
-rm -rf *
-
-#dowload wordpress (php-archive) from the git
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar 
 
-#download the lastest version of wordpress
 mv wp-cli.phar /usr/local/bin/wp
-wp core download --allow-root
+rm -f /var/www/html/wp-config.php
+cp ./wp-config.php /var/www/html/wp-config.php
 
-
-#change line in wpconfig to connect with the database
-mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
-sed -i -r "s/database/$MB_DATABASE/1" wp-config.php
-sed -i -r "s/database_user/$MB_USER/1" wp-config.php
-sed -i -r "s/passwod/$MB_PASSWORD/1" wp-config.php
-sed -i -r "s/localhost/mariadb/1" wp-config.php
-
-
-#install wordpress and sets up the basic config + creates new user + install astra theme for the site
-wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
-wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
-wp theme install astra --activate --allow-root
-sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+/usr/local/bin/wp core download --allow-root 
+/usr/local/bin/wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root 
+/usr/local/bin/wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
+/usr/local/bin/wp db check --allow-root
+sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php83/php-fpm.d/www.conf
 
 mkdir -p /run/php
-wp redis enable --allow-root
 
-#starts php service in the background
-/usr/sbin/php-fpm -F
+/usr/sbin/php-fpm83 -F
